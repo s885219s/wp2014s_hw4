30ccfe616fc4aec8e31e0d5089a2764e7cd48f91
s885219s <101703026@nccu.edu.tw>
2014-06-10 15:35:46 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -158,6 +158,7 @@ function PostImageToFacebook(authToken) {
     $('.info').append('<img src="img/loading.gif"/>')//載入loading的img
     var canvas = document.getElementById("canvas");//找canvas
     var imageData = canvas.toDataURL("image/png");//把canvas轉換PNG
+    var authToken = response.authResponse.accessToken; 
     try {
         blob = dataURItoBlob(imageData);//把影像載入轉換函數
     } catch (e) {
